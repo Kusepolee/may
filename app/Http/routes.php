cdcab13116a48d468fb0d64cf55746a262e9174a
Kris <me@restrose.net>
2016-06-06 11:00:55 +0800

    init
    init

--- app/Http/routes.php
+++ app/Http/routes.php
@@ -98,7 +98,7 @@ Route::get('/test', function () {
 	//echo sha1('king0105');
 	//echo sha1('king0105');
 	//echo hash_hmac('king0105');
-	//go on
+	//what's the fuck
 
 	echo hash_hmac("sha1", $json, 'king0105');
 	return response('200');
