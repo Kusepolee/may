d6e57fa3a33e9d0ae564aaaea268c6b40deff217
Kris <me@restrose.net>
2016-06-06 11:40:05 +0800

    init
    init

--- app/Http/routes.php
+++ app/Http/routes.php
@@ -28,7 +28,7 @@ Route::post('/login', 'MemberController@login');
 Route::get('/logout', 'MemberController@logout');
 
 //SERVER OK
-Route::post('/payload', 'ServerController@GithubWebhook');
+Route::any('/payload', 'ServerController@GithubWebhook');
 
 /*
 |--------------------------------------------------------------------------
