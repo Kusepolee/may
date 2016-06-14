68e6ad3f9924bc3a4dbc20eb4cd36120fea0df2a
Kris <me@restrose.net>
2016-06-14 12:25:42 +0800

    ok
    ok

--- resources/views/login.blade.php
+++ resources/views/login.blade.php
@@ -36,7 +36,7 @@ $err = $error[0].': '.$error[1];
         {!! Form::close() !!}
       </div>
     </div>
-      <!-- 错误信息 fuck-->
+      <!-- 错误信息 yeah-->
 
 	    @if($errors->any())  
 			<div class="panel-body">
